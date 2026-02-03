6a4cadd7732d6cf47e28d6c08762e39ff4ef9843
michaeliyer <161364287+michaeliyer@users.noreply.github.com>
2026-02-03 18:40:28 -0500

    Weigh Dealt With, Python Style

--- python-organized-3/available.py
+++ python-organized-3/available.py
@@ -116,14 +116,14 @@ unused_list = [
 	        "VALVE", "VAPOR", "VAUNT", "VEGAN", "VERSE", "VERSO",
 	        "VICAR", "VIGIL", "VIPER", "VIRUS", "VISIT", "VISTA",
 	        "VOCAL", "VOGUE", "VOMIT", "WAGER", 
-	        "WAIVE", "WARTY", "WAVER", "WEAVE", "WEIGH",
+	        "WAIVE", "WARTY", "WAVER", "WEAVE", 
 	        "WELCH", "WELSH", "WHARF", 
 	        "WIDER", "WIDOW", "WIGHT", "WILLY", "WIMPY", "WINCH", "WISER", "WISPY",
 	        "WOMEN", "WOODY", "WOOLY", "WOOZY", "WORTH", "WRACK", "WRECK", 
             "WREST", "WRING", "WRYLY", "ZONAL" 
              ]
 
-word = 'SPINY'
+word = 'WEIGH'
 
 
 if word in unused_list:
